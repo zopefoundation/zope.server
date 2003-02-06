4dd16b7980e7b601c692e7bb63efe0472f2c0466
Jeremy Hylton <jeremy@svn.zope.org>
2003-02-06 17:42:14 +0000

    Silence DeprecationWarning in Python 2.3: range() does not want floats.

--- maxsockets.py
+++ maxsockets.py
@@ -51,7 +51,7 @@ def max_select_sockets():
     while 1:
         try:
             num = len(sl)
-            for i in range(1 + len(sl) * 0.05):
+            for i in range(1 + len(sl) // 20):
                 # Increase exponentially.
                 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
                 s.bind (('',0))
