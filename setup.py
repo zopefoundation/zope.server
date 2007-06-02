93449825bf925258e846f6f88b9c88eca9d439f6
Philipp von Weitershausen <philipp@weitershausen.de>
2007-06-02 18:43:46 +0000

    bump version number after tagging

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.server',
-      version = '3.5.0a2',
+      version = '3.5.0a3',
       url='http://svn.zope.org/zope.server',
       license='ZPL 2.1',
       description='Zope server',
