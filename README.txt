75325fd488ba561133ae4d790374cff0e7e7745b
Stephan Richter <stephan.richter@gmail.com>
2008-02-02 11:27:09 +0000

    Improve package meta-data.

--- README.txt
+++ README.txt
@@ -1,13 +1,14 @@
 This package contains generic base classes for channel-based servers, the
 servers themselves and helper objects, such as tasks and requests.
 
-WSGI support
+============
+WSGI Support
 ============
 
-zope.server's HTTP server comes with WSGI_ support.
-``zope.server.http.wsgihttpserver.WSGIHTTPServer`` can act as a WSGI
-gateway.  There's also an entry point for PasteDeploy_ that lets you
-use zope.server's WSGI gateway from a configuration file, e.g.::
+`zope.server`'s HTTP server comes with WSGI_ support.
+``zope.server.http.wsgihttpserver.WSGIHTTPServer`` can act as a WSGI gateway.
+There's also an entry point for PasteDeploy_ that lets you use zope.server's
+WSGI gateway from a configuration file, e.g.::
 
   [server:main]
   use = egg:zope.server
@@ -16,34 +17,3 @@ use zope.server's WSGI gateway from a configuration file, e.g.::
 
 .. _WSGI: http://www.python.org/dev/peps/pep-0333/
 .. _PasteDeploy: http://pythonpaste.org/deploy/
-
-Changes
-=======
-
-3.5.? (2008-02-01)
-------------------
-
-- Fix of 599 error on conflict error in request
-  see: http://mail.zope.org/pipermail/zope-dev/2008-January/030844.html
-
-3.4.1 and 3.5.0a2 (2007-06-02)
-------------------------------
-
-Made WSGI server really WSGI-compliant by adding variables to the
-environment that are required by the spec.
-
-3.5.0a1 (2007-06-02)
---------------------
-
-Added a factory and entry point for PasteDeploy.
-
-3.4.0 (2007-06-02)
-------------------
-
-Removed an unused import. Unchanged otherwise.
-
-3.4.0a1 (2007-04-22)
---------------------
-
-Initial release as a separate project, corresponds to zope.server
-from Zope 3.4.0a1
--- setup.py
+++ setup.py
@@ -15,38 +15,52 @@
 
 $Id$
 """
-
 import os
-
 from setuptools import setup, find_packages
 
-setup(name='zope.server',
-      version = '3.5.0a3',
-      url='http://svn.zope.org/zope.server',
-      license='ZPL 2.1',
-      description='Zope server',
-      author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
-      long_description=open('README.txt').read(),
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
-
-      tests_require = ['zope.testing',
-                       'zope.i18n',
-                       'zope.component'],
-      install_requires = ['setuptools',
-                          'zope.interface',
-                          'zope.publisher',
-                          'zope.security',
-                          'zope.deprecation',
-                          'ZODB3'],
-      include_package_data = True,
-      zip_safe = False,
-      entry_points = """
-      [paste.server_runner]
-      main = zope.server.http.wsgihttpserver:run_paste
-      """
-      )
+setup(
+    name='zope.server',
+    version = '3.5.0dev',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Zope Server (Web and FTP)',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords=('zope3 server http ftp'),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://cheeseshop.python.org/pypi/zope.server',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['zope',],
+    tests_require = ['zope.testing',
+                     'zope.i18n',
+                     'zope.component'],
+    install_requires = ['setuptools',
+                        'zope.interface',
+                        'zope.publisher',
+                        'zope.security',
+                        'zope.deprecation',
+	                'ZODB3'],
+    include_package_data = True,
+    zip_safe = False,
+    entry_points = """
+    [paste.server_runner]
+    main = zope.server.http.wsgihttpserver:run_paste
+    """
+    )
