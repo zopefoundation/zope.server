43f01bd27df37f912d814ee9697559d0efc10b4a
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:26:15 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -29,7 +29,7 @@ def read(*rnames):
 setup(
     name='zope.server',
     version = '3.6.2dev',
-    author='Zope Corporation and Contributors',
+    author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Server (Web and FTP)',
     long_description=(
