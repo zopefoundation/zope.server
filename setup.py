9d5a5837ef54045cbd469e35a1a0dd03bb4233e0
Satchidanand Haridas <satchit@gmail.com>
2011-05-18 13:40:09 +0000

    updated CHANGES and vb in preparation for tagging.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ tests_require = [
 
 setup(
     name='zope.server',
-    version = '3.8.3dev',
+    version = '3.8.3',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Server (Web and FTP)',
