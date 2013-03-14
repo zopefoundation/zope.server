31a49a6f0f7dcf5e970906cc8859bfb37b884230
Stephan Richter <stephan.richter@gmail.com>
2013-03-13 21:40:01 -0400

    Preparing release 3.9.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ tests_require = [
 
 setup(
     name='zope.server',
-    version='3.9.0.dev',
+    version='3.9.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Server (Web and FTP)',
