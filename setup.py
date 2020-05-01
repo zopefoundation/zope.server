31840ca86500ff2d4d037de147f882838dd47c47
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-05-01 15:45:13 +0200

    Add python_requires to setup.py
    modified:   setup.py

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
     extras_require=dict(test=tests_require),
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     entry_points="""
         [paste.server_runner]
         main = zope.server.http.wsgihttpserver:run_paste
